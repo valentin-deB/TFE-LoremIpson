6ff229841efd8096b94cc08e44062cad998187fd
Valentin de Brabandère <valentindebrabandere@gmail.com>
2023-04-10 22:45:57 +0200

    Basic compoentn added + wop menu

---
+++ Tools/BasicComponent/BasicComponent.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./BasicComponent.scss";
+import "./styles/style-modernMac.scss";
+
+interface BasicComponentProps {
+  // add props here
+
+}
+
+const BasicComponent: React.FC<BasicComponentProps> = ({
+  // add props here
+
+}) => {
+  
+  // Component states and functions
+
+
+
+  //Component render
+
+  return (    
+      <>
+      </>
+  );
+};
+
+export default BasicComponent;
---
+++ src/components/Screen/assets/components/Menu/Menu.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import "./Menu.scss";
+import "./styles/style-modernMac.scss";
+
+// interface MenuProps {
+//   windowId: string;
+//   currentStyle: string;
+// }
+
+// const Menu: React.FC<MenuProps> = ({
+const Menu: React.FC = ({
+
+}) => {
+
+
+  //App render
+
+  return (    
+    <div className="c-menu">
+  );
+};
+
+export default Menu;
