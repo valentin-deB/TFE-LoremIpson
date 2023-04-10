77a2711c40df41247cf175449ecae6f0defb8379
Valentin de Brabandère <valentindebrabandere@gmail.com>
2023-04-10 23:08:55 +0200

    Menu display component

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,103 +1,21 @@
 import React from "react";
 import "./Screen.scss";
-import "./style/style-modernMac.scss";
+import "./styles/style-modernMac.scss";
+
+//assets
+import Menu from "./assets/components/Menu/Menu";
+
+//apps
 import Calculator from "../Apps/Calculator/Calculator";
 
-var style:string = "modernMac";
+var style:string = "style-modernMac";
 
 const Screen = () => {
   
   return(
         <div className="c-screen js-screen">
           <div className="c-screen__overlay js-screen__overlay"></div>
-          <div className="c-menu-bar">
-            <ul className="c-menu-bar__left">
-              <li className="c-menu-bar__item">
-                <button className="c-menu-bar__btn js-menu-bar__logo">
-                  <img
-                    className="c-menu-bar__icon"
-                    src={`assets/${style}/menuBarre/logoLoremIpson.png`}
-                    alt="logoLoremIpson"
-                  />
-                </button>
-              </li>
-              <li className="c-menu-bar__item c-menubar__application">
-                <button data-menu-bar="Files" className="c-menu-bar__btn">
-                  Files
-                </button>
-              </li>
-              <li className="c-menu-bar__item">
-                <button data-menu-bar="Edit" className="c-menu-bar__btn">
-                  Edit
-                </button>
-              </li>
-              <li className="c-menu-bar__item">
-                <button data-menu-bar="View" className="c-menu-bar__btn">
-                  View
-                </button>
-              </li>
-              <li className="c-menu-bar__item">
-                <button data-menu-bar="Go" className="c-menu-bar__btn">
-                  Go
-                </button>
-              </li>
-              <li className="c-menu-bar__item">
-                <button data-menu-bar="View" className="c-menu-bar__btn">
-                  Window
-                </button>
-              </li>
-              <li className="c-menu-bar__item">
-                <button data-menu-bar="Help" className="c-menu-bar__btn">
-                  Help
-                </button>
-              </li>
-            </ul>
-            <ul className="c-menu-bar__rigth">
-              <li className="c-menu-bar__item">
-                <button className="c-menu-bar__btn" data-menu-bar="Wifi">
-                  <img
-                    className="c-menu-bar__icon"
-                    src={`assets/${style}/menuBarre/wifiIcon.png`}
-                    alt="icon Wifi"
-                  />
-                </button>
-              </li>
-              <li className="c-menu-bar__item">
-                <button className="c-menu-bar__btn" data-menu-bar="Search">
-                  <img
-                    className="c-menu-bar__icon"
-                    src={`assets/${style}/menuBarre/searchIcon.png`}
-                    alt="icon Search"
-                  />
-                </button>
-              </li>
-              <li className="c-menu-bar__item">
-                <button
-                  className="c-menu-bar__btn"
-                  data-menu-bar="Control center"
-                >
-                  <img
-                    className="c-menu-bar__icon"
-                    src={`assets/${style}/menuBarre/controlCenterIcon.png`}
-                    alt="icon Control center"
-                  />
-                </button>
-              </li>
-              <li className="c-menu-bar__item" data-menu-bar="Date">
-                <button className="c-menu-bar__btn js-mennubar__btn--date c-mennubar__btn--date">
-                  Sat 25 Jun
-                </button>
-              </li>
-              <li className="c-menu-bar__item">
-                <button
-                  className="c-menu-bar__btn js-mennubar__btn--hour"
-                  data-menu-bar="Hour"
-                >
-                  17:13
-                </button>
-              </li>
-            </ul>
-          </div>
+          <Menu></Menu>
           <div className="o-container js-container">
             <Calculator windowId='1' currentStyle='ModernMac'/>
             <div className="c-desktop js-desktop">
@@ -112,7 +30,7 @@ const Screen = () => {
                 <div className="c-desktop__icon js-desktop__icon">
                   <img
                     className="c-desktop__icon-img js-desktop__icon-img o-fluidimage"
-                    src={`assets/${style}/desktopIcons/folder.png`}
+                    src={`./assets/${style}/desktopIcons/folder.png`}
                     alt="Folder"
                   />
                 </div>
@@ -164,7 +82,7 @@ const Screen = () => {
                 <div className="c-desktop__icon js-desktop__icon">
                   <img
                     className="c-desktop__icon-img js-desktop__icon-img o-fluidimage"
-                    src={`assets/${style}/desktopIcons/folder.png`}
+                    src={`./assets/${style}/desktopIcons/folder.png`}
                     alt="Folder"
                   />
                 </div>
@@ -180,7 +98,7 @@ const Screen = () => {
               >
                 <img
                   className="c-dock__icon js-dock__icon"
-                  src={`assets/${style}/dockIcons/finder.png`}
+                  src={`./assets/${style}/dockIcons/finder.png`}
                 />
               </div>
               <div
@@ -189,7 +107,7 @@ const Screen = () => {
               >
                 <img
                   className="c-dock__icon js-dock__icon"
-                  src={`assets/${style}/dockIcons/preferences.png`}
+                  src={`./assets/${style}/dockIcons/preferences.png`}
                 />
               </div>
               <div
@@ -198,7 +116,7 @@ const Screen = () => {
               >
                 <img
                   className="c-dock__icon js-dock__icon"
-                  src={`assets/${style}/dockIcons/appStore.png`}
+                  src={`./assets/${style}/dockIcons/appStore.png`}
                 />
               </div>
               <div
@@ -207,7 +125,7 @@ const Screen = () => {
               >
                 <img
                   className="c-dock__icon js-dock__icon"
-                  src={`assets/${style}/dockIcons/camera.png`}
+                  src={`./assets/${style}/dockIcons/camera.png`}
                 />
               </div>
               <div
@@ -216,7 +134,7 @@ const Screen = () => {
               >
                 <img
                   className="c-dock__icon js-dock__icon"
-                  src={`assets/${style}/dockIcons/photo.png`}
+                  src={`./assets/${style}/dockIcons/photo.png`}
                 />
               </div>
               <div
@@ -225,7 +143,7 @@ const Screen = () => {
               >
                 <img
                   className="c-dock__icon js-dock__icon"
-                  src={`assets/${style}/dockIcons/mail.png`}
+                  src={`./assets/${style}/dockIcons/mail.png`}
                 />
               </div>
               <div
@@ -234,7 +152,7 @@ const Screen = () => {
               >
                 <img
                   className="c-dock__icon js-dock__icon"
-                  src={`assets/${style}/dockIcons/calculator.png`}
+                  src={`./assets/${style}/dockIcons/calculator.png`}
                 />
               </div>
               <div
@@ -243,7 +161,7 @@ const Screen = () => {
               >
                 <img
                   className="c-dock__icon js-dock__icon"
-                  src={`assets/${style}/dockIcons/chrome.png`}
+                  src={`./assets/${style}/dockIcons/chrome.png`}
                 />
               </div>
               <div
@@ -252,7 +170,7 @@ const Screen = () => {
               >
                 <img
                   className="c-dock__icon js-dock__icon"
-                  src={`assets/${style}/dockIcons/figma.png`}
+                  src={`./assets/${style}/dockIcons/figma.png`}
                 />
               </div>
             </div>
@@ -263,7 +181,7 @@ const Screen = () => {
               >
                 <img
                   className="c-dock__icon js-dock__icon"
-                  src={`assets/${style}/dockIcons/trash.png`}
+                  src={`./assets/${style}/dockIcons/trash.png`}
                 />
               </div>
             </div>
--- src/components/Screen/assets/components/Menu/Menu.tsx
+++ src/components/Screen/assets/components/Menu/Menu.tsx
@@ -12,11 +12,101 @@ const Menu: React.FC = ({
 
 }) => {
 
+const [style, setStyle] = useState("style-modernMac");
 
   //App render
 
-  return (    
-    <div className="c-menu">
+  return (  
+        <>
+            <div className="c-menu-bar">
+            <ul className="c-menu-bar__left">
+              <li className="c-menu-bar__item">
+                <button className="c-menu-bar__btn js-menu-bar__logo">
+                  <img
+                    className="c-menu-bar__icon"
+                    src={`./assets/${style}/menuBarre/logoLoremIpson.png`}
+                    alt="logoLoremIpson"
+                  />
+                </button>
+              </li>
+              <li className="c-menu-bar__item c-menubar__application">
+                <button data-menu-bar="Files" className="c-menu-bar__btn">
+                  Files
+                </button>
+              </li>
+              <li className="c-menu-bar__item">
+                <button data-menu-bar="Edit" className="c-menu-bar__btn">
+                  Edit
+                </button>
+              </li>
+              <li className="c-menu-bar__item">
+                <button data-menu-bar="View" className="c-menu-bar__btn">
+                  View
+                </button>
+              </li>
+              <li className="c-menu-bar__item">
+                <button data-menu-bar="Go" className="c-menu-bar__btn">
+                  Go
+                </button>
+              </li>
+              <li className="c-menu-bar__item">
+                <button data-menu-bar="View" className="c-menu-bar__btn">
+                  Window
+                </button>
+              </li>
+              <li className="c-menu-bar__item">
+                <button data-menu-bar="Help" className="c-menu-bar__btn">
+                  Help
+                </button>
+              </li>
+            </ul>
+            <ul className="c-menu-bar__rigth">
+              <li className="c-menu-bar__item">
+                <button className="c-menu-bar__btn" data-menu-bar="Wifi">
+                  <img
+                    className="c-menu-bar__icon"
+                    src={`assets/${style}/menuBarre/wifiIcon.png`}
+                    alt="icon Wifi"
+                  />
+                </button>
+              </li>
+              <li className="c-menu-bar__item">
+                <button className="c-menu-bar__btn" data-menu-bar="Search">
+                  <img
+                    className="c-menu-bar__icon"
+                    src={`assets/${style}/menuBarre/searchIcon.png`}
+                    alt="icon Search"
+                  />
+                </button>
+              </li>
+              <li className="c-menu-bar__item">
+                <button
+                  className="c-menu-bar__btn"
+                  data-menu-bar="Control center"
+                >
+                  <img
+                    className="c-menu-bar__icon"
+                    src={`assets/${style}/menuBarre/controlCenterIcon.png`}
+                    alt="icon Control center"
+                  />
+                </button>
+              </li>
+              <li className="c-menu-bar__item" data-menu-bar="Date">
+                <button className="c-menu-bar__btn js-mennubar__btn--date c-mennubar__btn--date">
+                  Sat 25 Jun
+                </button>
+              </li>
+              <li className="c-menu-bar__item">
+                <button
+                  className="c-menu-bar__btn js-mennubar__btn--hour"
+                  data-menu-bar="Hour"
+                >
+                  17:13
+                </button>
+              </li>
+            </ul>
+          </div>
+        </>
   );
 };
 
