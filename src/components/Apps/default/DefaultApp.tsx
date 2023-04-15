cfcef088c79d6a0c5d7eeee89783a7c558c22a47
Valentin de Brabandère <valentindebrabandere@gmail.com>
2023-04-15 16:42:35 +0200

    Default app done

--- src/components/Apps/default/DefaultApp.tsx
+++ src/components/Apps/default/DefaultApp.tsx
@@ -17,7 +17,11 @@ const DefaultApp: React.FC<DefaultAppProps> = ({
 
   return (    
     <div className="c-default">
-      
+      <img className="c-default__icon" src={app.icon} alt="App icon" />
+      <h2 className="c-default__title">{name + " application is updating"}</h2>
+      <div className="c-default__animtxt">
+        <p>Please wait </p> <span className="c-default__point c-default__point--1">.</span> <span className="c-default__point c-default__point--2">.</span> <span className="c-default__point c-default__point--3">.</span>
+      </div>
     </div>
   );
 };
